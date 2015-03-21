fcaf2f9bea04c4e00bc62b1b2652efbd9d78df1a
Vinícius Gerevini <viniciusgerevini@gmail.com>
2015-03-21 02:45:02 -0300

    Converted tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,36 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		serve: {
-	        options: {
-	            port: 9000
-	        }
-	    },
-	    bower: {
-	    	install: {
+    grunt.initConfig({
+        serve: {
+            options: {
+                port: 9000
+            }
+        },
+        bower: {
+            install: {
 
-	    	}
-	    },
-	    vulcanize: {
-			default: {
-		    	options: {},
-		      	files: {
-		        	'build.html': 'index.html',
-		        	'demos_dist/binds.html': './demos/binds.html',
-		        	'demos_dist/event-handler.html': './demos/event-handler.html',
-		        	'demos_dist/lifecycle.html': './demos/lifecycle.html',
-		        	'demos_dist/simple-cards.html': './demos/simple-cards.html',
-		      	}
-		    }
-		}
-	});
+            }
+        },
+        vulcanize: {
+            default: {
+                options: {},
+                files: {
+                    'build.html': 'index.html',
+                    'demos_dist/binds.html': './demos/binds.html',
+                    'demos_dist/event-handler.html': './demos/event-handler.html',
+                    'demos_dist/lifecycle.html': './demos/lifecycle.html',
+                    'demos_dist/simple-cards.html': './demos/simple-cards.html',
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-serve');
-	grunt.loadNpmTasks('grunt-vulcanize');
-	grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-vulcanize');
+    grunt.loadNpmTasks('grunt-bower-task');
 
 
-	grunt.registerTask("default", ["bower", "serve"]);
+    grunt.registerTask("default", ["bower", "serve"]);
 
 
 
